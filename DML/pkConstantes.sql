91926f6682f2e64ec75cca6d3e20a5618c56e046
RigobertoBolanos <juandy46@hotmail.com>
2019-05-25 13:11:14 -0500

    pkConstantes
    Nivel 1

---
+++ DML/pkConstantes.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE PACKAGE pkConstantes as
+PROCEDURE pInsertar(ivNombreConstante VARCHAR2, ivValor VARCHAR2);
+PROCEDURE pBorrar(ivNombreConstante VARCHAR2);
+PROCEDURE pModificar(ivNombreConstante VARCHAR2, ivValor VARCHAR2);
+FUNCTION fConsultar(ivNombreConstante VARCHAR2) RETURN VARCHAR2;
+END pkConstantes;
+/
+
+CREATE OR REPLACE PACKAGE BODY pkConstantes AS
+
+PROCEDURE pInsertar(ivNombreConstante VARCHAR2, ivValor VARCHAR2)
+IS 
+BEGIN
+    INSERT INTO CONSTANTES VALUES (ivNombreConstante, ivValor);
+END pInsertar;
+
+PROCEDURE pBorrar(ivNombreConstante VARCHAR2)
+IS
+BEGIN
+    DELETE FROM CONSTANTES WHERE CONSTANTES.nombreConstante = ivNombreConstante;
+END pBorrar;
+
+PROCEDURE pModificar(ivNombreConstante VARCHAR2, ivValor VARCHAR2)
+IS
+BEGIN 
+    UPDATE CONSTANTES SET CONSTANTES.valor = ivValor WHERE CONSTANTES.nombreConstante = ivNombreConstante;  
+END pModificar;
+
+FUNCTION fConsultar(ivNombreConstante VARCHAR2) RETURN VARCHAR2
+IS
+CURSOR constanteBuscada IS 
+    SELECT *
+    FROM CONSTANTES 
+    WHERE CONSTANTES.nombreConstante = ivNombreConstante;
+ovNombreConstante VARCHAR2(20);
+ovValor VARCHAR2(20);
+BEGIN
+    OPEN constanteBuscada;
+    FETCH constanteBuscada INTO ovNombreConstante, ovValor;
+    CLOSE productoClienteBuscado;
+    
+    RETURN ovNombreConstante || ' ' || ovValor;
+END fConsultar;
+
+END pkConstantes;
