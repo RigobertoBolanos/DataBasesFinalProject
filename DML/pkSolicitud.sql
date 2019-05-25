f5d711eaa4260557c5281f907c93fce83077bcd8
Juan Manuel Imbachí G <41926056+JuanImbachi@users.noreply.github.com>
2019-05-25 12:01:58 -0500

    Modificación nombre métodos. Estándar enunciado

--- DML/pkSolicitud.sql
+++ DML/pkSolicitud.sql
@@ -1,37 +1,37 @@
 CREATE OR REPLACE PACKAGE pkSolicitud AS -- spec
-PROCEDURE pInsertarSolicitud(fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2);
-PROCEDURE pEliminarSolicitud (idSolicitud VARCHAR2);
-PROCEDURE pModificarSolicitud(fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2);
-FUNCTION fConsultarSolicitud(idSolicitud VARCHAR2) RETURN VARCHAR2;
+PROCEDURE pInsertar(fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2);
+PROCEDURE pBorrar(idSolicitud VARCHAR2);
+PROCEDURE pModificar(fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2);
+FUNCTION fConsultaridSolicitud VARCHAR2) RETURN VARCHAR2;
 END pkSolicitud;
 /
 
 CREATE OR REPLACE PACKAGE BODY pkSolicitud AS -- body
 
 -- Implementación Procedimiento 1
-PROCEDURE pInsertarSolicitud
+PROCEDURE pInsertar
 (fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2)
 IS
 BEGIN
     INSERT INTO SOLICITUD
     VALUES (fechaCreacion , fechaAsignacion , observaciones , estado , cliente_cedulaCliente , funcionario_cedulaFuncionario , tipoSolicitud , prodCli_codigoProducto , prodCli_cedulaCliente , idSolicitud );
-END pInsertarSolicitud;
+END pInsertar;
 
 -- Implementación Procedimiento 2
-PROCEDURE pEliminarSolicitud(idSolicitud VARCHAR2) IS
+PROCEDURE pBorrar(idSolicitud VARCHAR2) IS
 BEGIN
     DELETE FROM SOLICITUD WHERE Solicitud.idSolicitud = idSolicitud;
-END pEliminarSolicitud;
+END pBorrar;
 
 -- Implementación Procedimiento 3
-PROCEDURE pModificarSolicitud(fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2) IS
+PROCEDURE pModificar(fechaCreacion DATE, fechaAsignacion DATE, observaciones VARCHAR2, estado VARCHAR2, cliente_cedulaCliente VARCHAR2, funcionario_cedulaFuncionario VARCHAR2, tipoSolicitud VARCHAR2, prodCli_codigoProducto NUMBER, prodCli_cedulaCliente VARCHAR2, idSolicitud VARCHAR2) IS
 BEGIN
     UPDATE Solicitud SET Solicitud.fechaCreacion= fechaCreacion , Solicitud.fechaAsignacion = fechaAsignacion , Solicitud.observaciones = observaciones, Solicitud.estado = estado , Solicitud.cliente_cedulaCliente = cliente_cedulaCliente , Solicitud.funcionario_cedulaFuncionario = funcionario_cedulaFuncionario , Solicitud.tipoSolicitud = tipoSolicitud , Solicitud.prodCli_codigoProducto = prodCli_codigoProducto , Solicitud.prodCli_cedulaCliente = prodCli_cedulaCliente 
     WHERE Solicitud.idSolicitud=idSolicitud;
-END pModificarSolicitud;
+END pModificar;
 
 -- Implementación Procedimiento 4
-FUNCTION fConsultarSolicitud(idSolicitud VARCHAR2) return VARCHAR2 IS
+FUNCTION fConsultar(idSolicitud VARCHAR2) return VARCHAR2 IS
 vLista varchar2(100);
 CURSOR cSolicitudBuscada IS
     SELECT *
@@ -57,6 +57,6 @@ BEGIN
     
     RETURN idSolicitudC || ' ' || TO_CHAR(fechaCreacion) || ' ' || TO_CHAR(fechaAsignacion) || ' ' || estado || ' ' || cliente_cedulaCliente || ' ' || funcionario_cedulaFuncionario || ' ' || tipoSolicitud || ' ' || TO_CHAR(prodCli_codigoProducto) || ' ' || prodCli_cedulaCliente || ' ' || observaciones;
     
-END fConsultarSolicitud;
+END fConsultar;
 
 END pkSolicitud;
