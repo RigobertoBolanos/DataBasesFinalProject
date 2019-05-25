bb2e208bf51213e4f1790138ce174ed95b328838
Juan Manuel Imbachí G <41926056+JuanImbachi@users.noreply.github.com>
2019-05-24 20:11:42 -0500

    Correcciones pkProductoCliente

--- DML/pkProductoCliente.sql
+++ DML/pkProductoCliente.sql
@@ -23,7 +23,7 @@ END pBorrar;
 PROCEDURE pModificar(codigoProducto NUMBER, cliente_cedulacliente VARCHAR2, constantes_codigoconstante VARCHAR2)
 IS
 BEGIN 
-    UPDATE PRODCLI SET PRODCLI.codigoProducto = codigoProducto WHERE PRODCLI.codigoProducto = codigoProducto AND PRODCLI.cliente_cedulacliente = cedulaCliente;  
+    UPDATE PRODCLI SET PRODCLI.codigoProducto = codigoProducto WHERE PRODCLI.codigoProducto = codigoProducto AND PRODCLI.cliente_cedulacliente = cliente_cedulacliente;  
 END pModificar;
 
 FUNCTION fConsultar(codigoProducto NUMBER, cliente_cedulacliente VARCHAR2) RETURN VARCHAR2
@@ -31,16 +31,16 @@ IS
 CURSOR productoClienteBuscado IS 
     SELECT *
     FROM PRODCLI 
-    WHERE PRODCLI.codigoProducto = codigoProducto AND PRODCLI.cliente_cedulacliente = cedulaCliente;
-codigoProducto NUMBER(8);
-cliente_cedulacliente VARCHAR2(20);
+    WHERE PRODCLI.codigoProducto = codigoProducto AND PRODCLI.cliente_cedulacliente = cliente_cedulacliente;
+codigoProductoCliente NUMBER(8);
+cliente_cedulaclienteV VARCHAR2(20);
 constantes_codigoconstante VARCHAR2(20);
 BEGIN
     OPEN productoClienteBuscado;
-    FETCH productoClienteBuscado INTO codigoProducto, cliente_cedulacliente, constantes_codigoconstante;
-    CLOSE funcionarioBuscado;
+    FETCH productoClienteBuscado INTO codigoProductoCliente, cliente_cedulaclienteV, constantes_codigoconstante;
+    CLOSE productoClienteBuscado;
     
-    RETURN codigoProducto || ' ' || cliente_cedulacliente || ' ' || constantes_codigoconstante;
+    RETURN codigoProductoCliente || ' ' || cliente_cedulaclienteV || ' ' || constantes_codigoconstante;
 END fConsultar;
 
 END pkProductoCliente;
