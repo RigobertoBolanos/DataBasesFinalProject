350fc0a7673f24e206fc1b06abe7efe853797423
Juan Manuel Imbachí G <41926056+JuanImbachi@users.noreply.github.com>
2019-05-25 11:59:44 -0500

    Modificación nombre métodos. Estándar enunciado

--- DML/pkCliente.sql
+++ DML/pkCliente.sql
@@ -1,38 +1,38 @@
 CREATE OR REPLACE PACKAGE pkCliente AS -- spec
-PROCEDURE pInsertarCliente(cedula VARCHAR2, nombre VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2);
-PROCEDURE pEliminarCliente (cedula VARCHAR2);
-PROCEDURE pModificarCliente(nombre VARCHAR2, cedula VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2);
-FUNCTION fConsultarCliente(cedula VARCHAR2) RETURN VARCHAR2;
+PROCEDURE pInsertar(cedula VARCHAR2, nombre VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2);
+PROCEDURE pBorrar (cedula VARCHAR2);
+PROCEDURE pModificar(nombre VARCHAR2, cedula VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2);
+FUNCTION fConsultar(cedula VARCHAR2) RETURN VARCHAR2;
 END pkCliente;
 /
 
 CREATE OR REPLACE PACKAGE BODY pkCliente AS -- body
 
 -- Implementación Procedimiento 1
-PROCEDURE pInsertarCliente
+PROCEDURE pInsertar
 (cedula VARCHAR2, nombre VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2)
 IS
 BEGIN
     INSERT INTO Cliente
     VALUES (cedula, nombre, fechaNacimiento, direccion,
     telefono);
-END pInsertarCliente;
+END pInsertar;
 
 -- Implementación Procedimiento 2
-PROCEDURE pEliminarCliente(cedula VARCHAR2) IS
+PROCEDURE pBorrar(cedula VARCHAR2) IS
 BEGIN
     DELETE FROM Cliente WHERE Cliente.cedulaCliente = cedula;
-END pEliminarCliente;
+END pBorrar;
 
 -- Implementación Procedimiento 3
-PROCEDURE pModificarCliente(nombre VARCHAR2, cedula VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2) IS
+PROCEDURE pModificar(nombre VARCHAR2, cedula VARCHAR2, fechaNacimiento DATE, direccion VARCHAR2, telefono VARCHAR2) IS
 BEGIN
     UPDATE Cliente SET Cliente.nombre=nombre,Cliente.fechaNacimiento=fechaNacimiento,Cliente.direccion=direccion, Cliente.telefono=telefono
     WHERE Cliente.cedulaCliente=cedula;
-END pModificarCliente;
+END pModificar;
 
 -- Implementación Procedimiento 4
-FUNCTION fConsultarCliente(cedula VARCHAR2) RETURN VARCHAR2
+FUNCTION fConsultar(cedula VARCHAR2) RETURN VARCHAR2
 IS
 CURSOR cClienteSolicitado IS
     SELECT *
@@ -53,6 +53,6 @@ BEGIN
     
     RETURN cedulaCliente || ' ' || nombre || ' ' || TO_CHAR(fechaNacimiento) || ' ' || direccion || ' ' || telefono;
     
-END fConsultarCliente;
+END fConsultar;
 
 END pkCliente;
