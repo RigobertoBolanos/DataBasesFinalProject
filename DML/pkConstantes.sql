ab4bba7464f865b53534770e27c5b5f3068bfc18
RigobertoBolanos <juandy46@hotmail.com>
2019-05-25 16:35:10 -0500

    Correciones pkConssoli y pkConstante

--- DML/pkConstantes.sql
+++ DML/pkConstantes.sql
@@ -1,45 +1,47 @@
 CREATE OR REPLACE PACKAGE pkConstantes as
-PROCEDURE pInsertar(ivNombreConstante VARCHAR2, ivValor VARCHAR2);
-PROCEDURE pBorrar(ivNombreConstante VARCHAR2);
-PROCEDURE pModificar(ivNombreConstante VARCHAR2, ivValor VARCHAR2);
-FUNCTION fConsultar(ivNombreConstante VARCHAR2) RETURN VARCHAR2;
+PROCEDURE pInsertar(ivCodigoConstante VARCHAR2, ivNombreConstante VARCHAR2, ivValor VARCHAR2);
+PROCEDURE pBorrar(ivCodigoConstante VARCHAR2);
+PROCEDURE pModificar(ivCodigoConstante VARCHAR2,ivNombreConstante VARCHAR2, ivValor VARCHAR2);
+FUNCTION fConsultar(ivCodigoConstante VARCHAR2) RETURN VARCHAR2;
 END pkConstantes;
 /
 
 CREATE OR REPLACE PACKAGE BODY pkConstantes AS
 
-PROCEDURE pInsertar(ivNombreConstante VARCHAR2, ivValor VARCHAR2)
+PROCEDURE pInsertar(ivCodigoConstante VARCHAR2, ivNombreConstante VARCHAR2, ivValor VARCHAR2)
 IS 
 BEGIN
-    INSERT INTO CONSTANTES VALUES (ivNombreConstante, ivValor);
+    INSERT INTO CONSTANTES VALUES (ivCodigoConstante, ivNombreConstante, ivValor);
 END pInsertar;
 
-PROCEDURE pBorrar(ivNombreConstante VARCHAR2)
+PROCEDURE pBorrar(ivCodigoConstante VARCHAR2)
 IS
 BEGIN
-    DELETE FROM CONSTANTES WHERE CONSTANTES.nombreConstante = ivNombreConstante;
+    DELETE FROM CONSTANTES WHERE CONSTANTES.codigoConstante = ivCodigoConstante;
 END pBorrar;
 
-PROCEDURE pModificar(ivNombreConstante VARCHAR2, ivValor VARCHAR2)
+PROCEDURE pModificar(ivCodigoConstante VARCHAR2,ivNombreConstante VARCHAR2, ivValor VARCHAR2)
 IS
 BEGIN 
-    UPDATE CONSTANTES SET CONSTANTES.valor = ivValor WHERE CONSTANTES.nombreConstante = ivNombreConstante;  
+    UPDATE CONSTANTES SET CONSTANTES.valor = ivValor WHERE CONSTANTES.codigoConstante = ivCodigoConstante;  
+    UPDATE CONSTANTES SET CONSTANTES.nombreConstante = ivNombreConstante WHERE CONSTANTES.codigoConstante = ivCodigoConstante; 
 END pModificar;
 
-FUNCTION fConsultar(ivNombreConstante VARCHAR2) RETURN VARCHAR2
+FUNCTION fConsultar(ivCodigoConstante VARCHAR2) RETURN VARCHAR2
 IS
 CURSOR cuConstanteBuscada IS 
     SELECT *
     FROM CONSTANTES 
-    WHERE CONSTANTES.nombreConstante = ivNombreConstante;
+    WHERE CONSTANTES.codigoConstante = ivCodigoConstante;
+ovCodigoConstante VARCHAR(2);
 ovNombreConstante VARCHAR2(20);
 ovValor VARCHAR2(20);
 BEGIN
     OPEN cuConstanteBuscada;
-    FETCH cuConstanteBuscada INTO ovNombreConstante, ovValor;
+    FETCH cuConstanteBuscada INTO ovCodigoConstante,ovNombreConstante, ovValor;
     CLOSE cuConstanteBuscada;
     
-    RETURN ovNombreConstante || ' ' || ovValor;
+    RETURN ovCodigoConstante || ' ' || ovNombreConstante || ' ' || ovValor;
 END fConsultar;
 
 END pkConstantes;
--- DML/pkConstantesSolicitud.sql
+++ DML/pkConstantesSolicitud.sql
@@ -1,46 +1,46 @@
 CREATE OR REPLACE PACKAGE pkConstantesSolicitud as
-PROCEDURE pInsertar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2);
-PROCEDURE pBorrar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2);
-PROCEDURE pModificar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2);
-FUNCTION fConsultar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2) RETURN VARCHAR2;
+PROCEDURE pInsertar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2);
+PROCEDURE pBorrar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2);
+PROCEDURE pModificar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2);
+FUNCTION fConsultar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2) RETURN VARCHAR2;
 END pkConstantesSolicitud;
 /
 
 CREATE OR REPLACE PACKAGE BODY pkConstantesSolicitud AS
 
-PROCEDURE pInsertar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2)
+PROCEDURE pInsertar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2)
 IS 
 BEGIN
-    INSERT INTO CONSSOLI VALUES (ivSolicitud_idsolicitud, ivConstantes_nombreconstante);
+    INSERT INTO CONSSOLI VALUES (ivSolicitud_idsolicitud, ivConstantes_codigoConstante);
 END pInsertar;
 
-PROCEDURE pBorrar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2)
+PROCEDURE pBorrar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2)
 IS
 BEGIN
-    DELETE FROM CONSSOLI WHERE CONSSOLI.solicitud_idsolicitud = ivSolicitud_idsolicitud AND CONSSOLI.constantes_nombreconstante = ivConstantes_nombreconstante;
+    DELETE FROM CONSSOLI WHERE CONSSOLI.solicitud_idsolicitud = ivSolicitud_idsolicitud AND CONSSOLI.constantes_codigoconstante = ivConstantes_codigoConstante;
 END pBorrar;
 
-PROCEDURE pModificar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2)
+PROCEDURE pModificar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2)
 IS
 BEGIN 
     UPDATE CONSSOLI SET CONSSOLI.solicitud_idsolicitud = ivSolicitud_idsolicitud WHERE CONSSOLI.solicitud_idsolicitud = ivSolicitud_idsolicitud; 
-    UPDATE CONSSOLI SET CONSSOLI.constantes_nombreconstante = ivConstantes_nombreconstante WHERE CONSSOLI.constantes_nombreconstante = ivConstantes_nombreconstante; 
+    UPDATE CONSSOLI SET CONSSOLI.constantes_nombreconstante = ivConstantes_codigoConstante WHERE CONSSOLI.constantes_codigoconstante = ivConstantes_codigoConstante; 
 END pModificar;
 
-FUNCTION fConsultar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_nombreconstante VARCHAR2) RETURN VARCHAR2
+FUNCTION fConsultar(ivSolicitud_idsolicitud VARCHAR2, ivConstantes_codigoConstante VARCHAR2) RETURN VARCHAR2
 IS
 CURSOR cuConstSolicitudBuscada IS 
     SELECT *
     FROM CONSSOLI 
-    WHERE CONSSOLI.solicitud_idsolicitud = ivSolicitud_idsolicitud AND CONSSOLI.constantes_nombreconstante = ivConstantes_nombreconstante;
+    WHERE CONSSOLI.solicitud_idsolicitud = ivSolicitud_idsolicitud AND CONSSOLI.constantes_codigoconstante = ivConstantes_codigoConstante;
 ovSolicitud_idsolicitud        VARCHAR2(8);
-ovConstantes_nombreconstante   VARCHAR2(20);
+ovConstantes_codigoConstante   VARCHAR2(20);
 BEGIN
     OPEN cuConstSolicitudBuscada;
-    FETCH cuConstSolicitudBuscada INTO ovSolicitud_idsolicitud, ovConstantes_nombreconstante;
+    FETCH cuConstSolicitudBuscada INTO ovSolicitud_idsolicitud, ovConstantes_codigoConstante;
     CLOSE cuConstSolicitudBuscada;
     
-    RETURN ovSolicitud_idsolicitud || ' ' || ovConstantes_nombreconstante;
+    RETURN ovSolicitud_idsolicitud || ' ' || ovConstantes_codigoConstante;
 END fConsultar;
 
 END pkConstantesSolicitud;
